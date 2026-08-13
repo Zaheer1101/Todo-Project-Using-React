7152b4b052cac845b18ae277cafc6cdf906029be
Zaheer <zaheerahmedchikkanarti@gmail.com>
2026-08-14 00:39:12 +0530

    Improve header and footer UI

--- src/contents/Footer.jsx
+++ src/contents/Footer.jsx
@@ -2,10 +2,31 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <br />
-      <p>© 2026 Advanced To-Do App</p>
-    </footer>
+    <footer className="simple-footer">
+  <p>
+    © 2026 <strong>Advanced To-Do App</strong>
+    <span> • </span>
+    Developed by <strong>ZaheerAhmed</strong>
+  </p>
+
+  <div className="footer-links">
+    <a
+      href="https://github.com/Zaheer1101"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      GitHub
+    </a>
+
+    <a
+      href="https://www.linkedin.com/in/zaheerahmed-chikkanarti-3aab6027b/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      LinkedIn
+    </a>
+  </div>
+</footer>
   );
 };
 
--- src/contents/Header.jsx
+++ src/contents/Header.jsx
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <header>
       <h1>🚀 Advanced To-Do App</h1>
+      <p>Todo Project Using React</p>
       <p>Stay organized and productive</p>
     </header>
   );
